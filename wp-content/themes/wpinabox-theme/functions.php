c4d606565158b70538bf7d226880f1a63816b8fd
Andy Dayton <amdayton@gmail.com>
2017-02-18 16:10:53 -0500

    Test things out with basic theme

--- wp-content/themes/wpinabox-theme/functions.php
+++ wp-content/themes/wpinabox-theme/functions.php
@@ -8,16 +8,11 @@ $function_includes = [
   'lib/timber.php',
   'lib/utilities.php',
   'lib/admin.php',
-  'lib/post-types/image.php',
-  'lib/post-types/brand.php',
-  'lib/post-types/person.php',
-  'lib/post-types/press.php',
-  'lib/post-types/workshop.php',
   'lib/post-types/page.php'
 ];
 foreach ($function_includes as $file) {
   if (!$filepath = locate_template($file)) {
-    trigger_error(sprintf(__('Error locating %s for inclusion', 'sage'), $file), E_USER_ERROR);
+    trigger_error(sprintf(__('Error locating %s for inclusion', 'wpinabox'), $file), E_USER_ERROR);
   }
   require_once $filepath;
 }
