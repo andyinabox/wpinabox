38de3ed17328bfd45437f1aeecd70db1d933a8a6
Andy Dayton <amdayton@gmail.com>
2017-03-01 21:06:20 -0500

    Fixed init script

--- bin/init.sh
+++ bin/init.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
 
+if [ "$(whoami)" != "vagrant" ]; then
+   echo "This script must be run as vagrant user. vagrant ssh and try again." 1>&2
+   exit 1
+fi
+
 read -p "Enter a replace value for project name (My Project Name): " proj_name
 read -p "Enter a lower-case replace value for namespace (mpn): " proj_ns_lower
 read -p "Enter an upper-case replace value for namespace (MPN): " proj_ns_upper
 read -p "Enter file-friendly project name (my-project-name): " proj_name_lower
-read -p "What's your ACF Pro API key?" acf_pro_key
+read -p "What's your ACF Pro API key? " acf_pro_key
 
 theme_name="$proj_name_lower-theme"
 replace_path="./wp-content/themes/$theme_name"
 
 echo "renaming theme to $theme_name"
 mv wp-content/themes/wpinabox-theme "$replace_path"
-
 
 echo "replacing names..."
 find $replace_path -type f -readable -writable -exec sed -i "s/WPinabox/$proj_name/g" {} \;
