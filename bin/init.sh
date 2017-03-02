ae896e1ac7e22703a8022b6e8bdadab9c4468d0d
Andy Dayton <amdayton@gmail.com>
2017-03-01 20:58:22 -0500

    Update init script

--- bin/init.sh
+++ bin/init.sh
@@ -6,28 +6,27 @@ read -p "Enter an upper-case replace value for namespace (MPN): " proj_ns_upper
 read -p "Enter file-friendly project name (my-project-name): " proj_name_lower
 read -p "What's your ACF Pro API key?" acf_pro_key
 
-theme_name="themes/$proj_name_lower-theme" 
+theme_name="$proj_name_lower-theme"
+replace_path="./wp-content/themes/$theme_name"
 
-cd ./wp-content
+echo "renaming theme to $theme_name"
+mv wp-content/themes/wpinabox-theme "$replace_path"
+
 
 echo "replacing names..."
+find $replace_path -type f -readable -writable -exec sed -i "s/WPinabox/$proj_name/g" {} \;
+find $replace_path -type f -readable -writable -exec sed -i "s/wpb/$proj_ns_lower/g" {} \;
+find $replace_path -type f -readable -writable -exec sed -i "s/WPB/$proj_ns_upper/g" {} \;
+find $replace_path -type f -readable -writable -exec sed -i "s/wpinabox/$proj_name_lower/g" {} \;
 
-grep -ilr 'WPinabox' * | xargs -I@ sed -i '' "s/WPinabox/$proj_name/g" @
-grep -ilr 'wpb' * | xargs -I@ sed -i '' "s/wpb/$proj_ns_lower/g" @
-grep -ilr 'WPB' * | xargs -I@ sed -i '' "s/WPB/$proj_ns_upper/g" @
-grep -ilr 'wpinabox' * | xargs -I@ sed -i '' "s/wpinabox/$proj_name_lower/g" @
-mv "themes/wpinabox-theme" "themes/$proj_name_lower-theme"
-
-cd -
-
-echo "installing composer dependencies"
+echo "installing composer dependencies..."
 echo "ACF_PRO_KEY=$acf_pro_key" > .env
 composer install
 
-echo "activating theme and plugins"
+echo "activating theme and plugins..."
 wp plugin activate advanced-custom-fields-pro
 wp plugin activate timber-library
 wp theme activate "$theme_name"
 
-echo "removing git repository"
-# rm -rf .git
+echo "removing git repository..."
+rm -rf .git
