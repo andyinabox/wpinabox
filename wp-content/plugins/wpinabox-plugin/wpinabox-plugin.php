d24254dc6dd7d1077d00ec1673f0d02925b590bf
amdayton <amdayton@gmail.com>
2018-09-10 16:10:28 -0500

    Updates to theme/plugin bootstrapping

--- wp-content/plugins/wpinabox-plugin/wpinabox-plugin.php
+++ wp-content/plugins/wpinabox-plugin/wpinabox-plugin.php
@@ -2,17 +2,23 @@
 // namespace Even;
 
 /**
- * Plugin Name:     Even Plugin
+ * Plugin Name:     Wpinabox Plugin
  * Plugin URI:      -
- * Description:     Plugin for even magazine
- * Author:          Andy Dayton for Common Name
- * Author URI:      http://common-name.com
- * Text Domain:     even-plugin
+ * Description:     Plugin template
+ * Author:          Andy Dayton
+ * Author URI:      http://andydayton.com
+ * Text Domain:     wpinabox
  * Domain Path:     /languages
- * Version:         0.1.0
+ * Version:         0.2.0
  *
- * @package         Even_Plugin
+ * @package         WPB
  */
+
+require_once __DIR__ . '/../../../vendor/autoload.php';
+
+// Init Dotenv
+$dotenv = new Dotenv\Dotenv(__DIR__ . '/../../../');
+$dotenv->load();
 
 $timber_path = plugin_dir_path( __FILE__ ).'../timber-library/timber.php';
 
@@ -34,17 +40,17 @@ if(!class_exists('Timber')) {
 }
 
 // taxonomies
-// include_once 'lib/taxonomies/example.php';
+// include_once 'app/taxonomies/example.php';
 
   // post types
-include_once 'lib/post-types/base-post.php';
-include_once 'lib/post-types/post.php';
-include_once 'lib/post-types/page.php';
-// include_once 'lib/post-types/example.php';
+include_once 'app/post-types/base-post.php';
+include_once 'app/post-types/post.php';
+include_once 'app/post-types/page.php';
+// include_once 'app/post-types/example.php';
 
 // misc
-include_once 'lib/shortcodes.php';
-include_once 'lib/helper.php';
-include_once 'lib/plugin.php';
+include_once 'app/shortcodes.php';
+include_once 'app/helper.php';
+include_once 'app/plugin.php';
 
 new WPB\Plugin();
--- wp-content/themes/wpinabox-theme/functions.php
+++ wp-content/themes/wpinabox-theme/functions.php
@@ -1,25 +1,3 @@
 <?php
 
-/**
- * Make sure Timber library is installed
- */
-if ( ! class_exists( 'Timber' ) ) {
-	add_action( 'admin_notices', function() {
-		echo '<div class="error"><p>Timber not activated. Make sure you activate the plugin in <a href="' . esc_url( admin_url( 'plugins.php#timber' ) ) . '">' . esc_url( admin_url( 'plugins.php') ) . '</a></p></div>';
-	});
-	
-	add_filter('template_include', function($template) {
-		return get_stylesheet_directory() . '/assets/no-timber.html';
-	});
-	
-	return;
-}
-
-
-// set template directory
-Timber::$dirname = array('tpl');
-
-// includes
-require_once 'lib/site.php';
-
-?>
+require_once __DIR__ . '/app/bootstrap.php';
